4497680f9ebba5128f0c4eaa24640c503134d06c
Zuzeth Romo <zuzethromo@yahoo.com>
2019-09-20 15:07:40 -0600

    handlebars

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 let express = require("express");
+let mongoose = require("mongoose");
+let expressHandlebars = require("express-handlebars");
+let bodyParser = require("body-parser");
+
 let PORT = process.env.PORT || 3000;
 let app = express();
 let router = express.Router();
 
 app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars ({
+    defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded ({
+    extended: false
+}));
+
 app.use(router);
+
+let db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 app.listen(PORT, function () {
     console.log ("Listening on port:" + PORT)
