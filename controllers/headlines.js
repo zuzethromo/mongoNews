4ded786b1dadff9c68bf3bdef4bf0d9b85ba0879
Zuzeth Romo <zuzethromo@yahoo.com>
2019-09-20 17:51:35 -0600

    controllers

---
+++ controllers/headlines.js
@@ -0,0 +1,38 @@
+let scrape = require("../scripts/scrape");
+let makeDate = require("../scripts/date");
+
+let Headline = require("../models/Headline");
+
+module.exports = {
+    fetch: function(cb) {
+        scrape(function(data) {
+            let articles = data;
+            
+            for(let i=0; i < articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+
+            Headline.collection.insertMany(articles, {ordered: false}, function(err, docs) {
+                cb(err, docs);
+            });
+        });
+    },
+    delete: function(query, cb) {
+        Headline.remove(query, cb);
+    },
+    get: function(query, cb) {
+        Headline.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(err, doc) {
+            cb(doc);
+        });
+    },
+    update: function(query, cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        }, {}, cb);
+    }
+}
